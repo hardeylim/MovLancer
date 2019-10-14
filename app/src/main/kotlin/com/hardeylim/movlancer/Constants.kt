0c047b10fde013dfd0fd5653816711d44f39258b
Hardey Lim <hardeylim@gmail.com>
2019-10-14 16:41:42 +0800

    Add Constants Named Injections

---
+++ app/src/main/kotlin/com/hardeylim/movlancer/Constants.kt
@@ -0,0 +1,9 @@
+package com.hardeylim.movlancer
+
+class Constants {
+    object NamedInjections {
+        const val INTERNET_CHECKER = "InternetChecker"
+        const val MOVIES_DB = "MoviesDB"
+    }
+
+}
