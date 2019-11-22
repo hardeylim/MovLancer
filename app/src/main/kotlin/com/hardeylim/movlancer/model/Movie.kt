7cda75c3ef6befcb8b29ba529e9c2aca7f862623
Hardey <hardeylim@gmail.com>
2019-11-22 17:44:20 +0800

    Change val to var to be reassigned
    Notes: contemplating whether changing data classes to var is right or I should've just made it a normal class. To be updated later on.

--- app/src/main/kotlin/com/hardeylim/movlancer/model/Movie.kt
+++ app/src/main/kotlin/com/hardeylim/movlancer/model/Movie.kt
@@ -4,31 +4,31 @@ import com.google.gson.annotations.SerializedName
 
 data class Movie(
         @SerializedName("popularity")
-        val popularity: Double = 0.0, // 66.895
+        var popularity: Double = 0.0, // 66.895
         @SerializedName("vote_count")
-        val voteCount: Int = 0, // 256
+        var voteCount: Int = 0, // 256
         @SerializedName("video")
-        val video: Boolean = false, // false
+        var video: Boolean = false, // false
         @SerializedName("poster_path")
-        val posterPath: String = "", // /y9IcwcD95wCPR3IGbzlZ00f64Sj.jpg
+        var posterPath: String = "", // /y9IcwcD95wCPR3IGbzlZ00f64Sj.jpg
         @SerializedName("id")
-        val id: Int = 0, // 540901
+        var id: Int = 0, // 540901
         @SerializedName("adult")
-        val adult: Boolean = false, // false
+        var adult: Boolean = false, // false
         @SerializedName("backdrop_path")
-        val backdropPath: String = "", // /5GynP6w2OQWSbKnCLHrBIriF4Cw.jpg
+        var backdropPath: String = "", // /5GynP6w2OQWSbKnCLHrBIriF4Cw.jpg
         @SerializedName("original_language")
-        val originalLanguage: String = "", // en
+        var originalLanguage: String = "", // en
         @SerializedName("original_title")
-        val originalTitle: String = "", // Hustlers
+        var originalTitle: String = "", // Hustlers
         @SerializedName("genre_ids")
-        val genreIds: List<Int> = listOf(),
+        var genreIds: List<Int> = listOf(),
         @SerializedName("title")
-        val title: String = "", // Hustlers
+        var title: String = "", // Hustlers
         @SerializedName("vote_average")
-        val voteAverage: Double = 0.0, // 6.2
+        var voteAverage: Double = 0.0, // 6.2
         @SerializedName("overview")
-        val overview: String = "", // A crew of savvy former strip club employees band together to turn the tables on their Wall Street clients.
+        var overview: String = "", // A crew of savvy former strip club employees band together to turn the tables on their Wall Street clients.
         @SerializedName("release_date")
-        val releaseDate: String = "" // 2019-09-13
-    )
+        var releaseDate: String = "" // 2019-09-13
+)
