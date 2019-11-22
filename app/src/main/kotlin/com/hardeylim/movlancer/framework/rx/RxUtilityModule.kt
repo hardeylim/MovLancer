a49041d3bb9dc160461282abb19af36e3f4b53ea
Hardey <hardeylim@gmail.com>
2019-11-22 17:16:48 +0800

    Add rx scheduler utils provider

---
+++ app/src/main/kotlin/com/hardeylim/movlancer/framework/rx/RxUtilityModule.kt
@@ -0,0 +1,17 @@
+package com.hardeylim.movlancer.framework.rx
+
+
+import dagger.Module
+import dagger.Provides
+
+import javax.inject.Singleton
+
+@Module
+class RxUtilityModule {
+
+    @Provides
+    @Singleton
+    internal fun provideRxSchedulerUtils(): RxSchedulerUtils {
+        return DefaultRxSchedulerUtils()
+    }
+}
